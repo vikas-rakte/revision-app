4215cd110c2f6fea9ef9ff12e1a7809554a9f88a
vikas-rakte <vikasrakte@gmail.com>
2026-03-20 23:23:14 +0530

    fix name

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./store.ts";
 import { QueryClientProvider } from "@tanstack/react-query";
-import queryClient from "./queryClient.ts";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import queryClient from "./query.client.ts";
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
